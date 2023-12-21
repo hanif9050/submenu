08388a289ac73a68d75b2b440da8639f78016b9b
hanif9050 <hanif.ctg9050@gmail.com>
2023-12-21 14:28:13 +0600

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
+import { useGlobalContext } from "./Context";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Submenu from "./Submenu";
+
 const App = () => {
-  return <h2>Strapi Starter</h2>;
+ 
+  return <main>
+    <Navbar />
+    <Hero />
+    <Sidebar />
+    <Submenu hello="hello"/>
+  </main>;
+
 };
 export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {FaBars} from "react-icons/fa";
+import { useGlobalContext } from './Context';
+import NavLinks from './NavLinks';
+const Navbar = () => {
+const {openSidebar,setPageId} = useGlobalContext();
+const handleSubmit=(e)=>{
+console.log(e.target)
+if(!e.target.classList.contains("nav-link")){
+  setPageId(null)
+}
+}
+  return (
+    <nav onMouseOver={handleSubmit}>
+      <div className="nav-center">
+        <h3 className='logo'>strapi</h3>
+        <button className="toggle-btn" onClick={openSidebar}>
+        <FaBars />
+        </button>
+        {/* nav links later */}
+        <NavLinks />
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
